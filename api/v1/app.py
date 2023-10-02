a8bc649d5ca7cc20b4f7aff3ec19dbb70663bdc1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-10-02 22:19:40 +0000

    CORS for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def remove_session(exception):
     """ remove the current sql alchemy session """
